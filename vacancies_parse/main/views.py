30c1f2cade59a1426a7084ad105b5ee1e72a51b8
maria <surkina05@mail.ru>
2024-12-27 23:57:19 +0500

    update main page, create html/css

--- vacancies_parse/main/views.py
+++ vacancies_parse/main/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>HELLO WORLD</h1>")
+    return render(request, 'main/index.html')
