d7ad0e07cc21ace53de4287e20360fd5afa4858f
maria <surkina05@mail.ru>
2025-01-12 02:41:34 +0500

    update general statistics. uploading graphs and html-table-statistics to page. CSS+HTML

--- vacancies_parse/main/views.py
+++ vacancies_parse/main/views.py
@@ -10,10 +10,11 @@ def general_statistics_view(request):
     # Получаем объект GeneralStatistics, например, с id=1
     general_statistics = GeneralStatistics.objects.get(id=1)
 
-    # Получаем первый раздел (если он существует)
-    first_section = general_statistics.sections.first()
+    # Получаем все разделы, связанные с объектом GeneralStatistics
+    sections = general_statistics.sections.all()
 
     # Передаем данные в шаблон
     return render(request, 'main/general_statistics.html', {
-        'first_section': first_section
+        'sections': sections
     })
+
